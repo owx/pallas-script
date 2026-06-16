7039f81abaeb27d63af40411ffe2f385e5cf2f4b
Cary <logcr@163.com>
2026-06-16 09:34:41 +0800

    udpdate

--- src/apps/xbox/hardware/radar/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { startService } from "./core.js";
-
-export function mrMain(){
-    startService();
-}
--- src/apps/xbox/quality/keywords-scan/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { startScan } from "./scan-engine.js";
-import { main  } from './sensitive-analysis.js'
-// startScan();
-
-main();
